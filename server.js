a79d0de3d16a1c9dbe7e7f66f5a6bf80c592f84f
sale-win4web-dpdns-org <sale@win4web.dpdns.org>
2025-05-14 02:58:08 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import fetch from 'node-fetch';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const app = express();
-const port = process.env.PORT || 10000;
+const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+// __dirname replacement in ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// Serve static files
+app.use(express.static(__dirname));
+
+// Define target URL
+const targetUrl = 'https://cmxbcqguzbnu.eu-central-1.clawcloudrun.com/';
+
+app.get('/', async (req, res) => {
+  try {
+    const response = await fetch(targetUrl, { method: 'HEAD' });
+    if (response.ok) {
+      res.redirect(targetUrl);
+    } else {
+      res.sendFile(path.join(__dirname, 'index2.html'));
+    }
+  } catch (err) {
+    res.sendFile(path.join(__dirname, 'index2.html'));
+  }
 });
 
 app.listen(port, () => {
